c28eb03223646fc9ec2b0530a65781a2ab19c6fa
Milton Angel Martinez Rodriguez <martinez.rodriguez.miltonangel@usfx.bo>
2024-01-16 16:43:20 -0400

    actualizacion get

---
+++ src/main/java/bo/usfx/Web/Services/Country/controller/CountryController.java
@@ -0,0 +1,34 @@
+package bo.usfx.Web.Services.Country.controller;
+
+import bo.usfx.Web.Services.Country.model.Country;
+import bo.usfx.Web.Services.Country.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/countrys")
+    public List<Country> getAll() {
+        return countryRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/v1/countrys/{id}")
+    public ResponseEntity<?> getById(@PathVariable String id) {
+        var country = countryRepository.findById(id);
+        if (country != null) {
+            return ResponseEntity.ok(country);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/bo/usfx/Web/Services/Country/model/Country.java
@@ -0,0 +1,37 @@
+package bo.usfx.Web.Services.Country.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "countries")
+public class Country {
+
+    @Id
+    private String id;
+    private String name;
+    private Integer population;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
