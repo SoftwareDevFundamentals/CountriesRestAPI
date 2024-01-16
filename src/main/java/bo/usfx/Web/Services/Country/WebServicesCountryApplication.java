7c5ffd6685844dd9f5997c760637a28294f69f00
brayan146 <brayan14717@gmail.com>
2024-01-15 22:54:10 -0400

    first commit

---
+++ src/main/java/bo/usfx/Web/Services/Country/WebServicesCountryApplication.java
@@ -0,0 +1,13 @@
+package bo.usfx.Web.Services.Country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesCountryApplication.class, args);
+	}
+
+}
