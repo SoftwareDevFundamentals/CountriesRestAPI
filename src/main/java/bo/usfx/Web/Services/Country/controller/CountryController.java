2e7cf25b7f00873313f6d459f909e250d632d489
Milton Angel Martinez Rodriguez <martinez.rodriguez.miltonangel@usfx.bo>
2024-01-17 09:40:20 -0400

    actualizacion 17 enero

--- src/main/java/bo/usfx/Web/Services/Country/controller/CountryController.java
+++ src/main/java/bo/usfx/Web/Services/Country/controller/CountryController.java
@@ -17,13 +17,20 @@ public class CountryController {
     @Autowired
     private CountryRepository countryRepository;
 
-    // Gets the list of all countries
+    /**
+     *  Gets the list of all countries
+     *  @return List of countries
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/api/v1/countries")
     public List<Country> getAll() {
         return countryRepository.findAll();
     }
 
-    // Gets an element from the list of all countries with the id
+    /**
+     *  Gets an element from the list of all countries with the id
+     *  @param id Identificador del pais
+     *  @return country
+     */
     @RequestMapping(method = RequestMethod.GET, value = "api/v1/countries/{id}")
     public ResponseEntity<?> getById(@PathVariable final String id) {
         var country = countryRepository.findById(id);
