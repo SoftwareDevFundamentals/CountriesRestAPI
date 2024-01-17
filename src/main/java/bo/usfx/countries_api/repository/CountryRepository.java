457e5a115702dff4efa2a677096ef66d5eece77c
brayan146 <brayan14717@gmail.com>
2024-01-17 11:07:34 -0400

    bug fixes

--- src/main/java/bo/usfx/countries_api/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package bo.usfx.countries_api.repository;
-
-import bo.usfx.countries_api.model.Country;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CountryRepository extends MongoRepository<Country, String> {
-}
